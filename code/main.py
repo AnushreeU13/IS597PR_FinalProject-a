53e50383654f3151e5df05b3f602e3bb3a2f7ac8
AnushreeU13 <85550745+AnushreeU13@users.noreply.github.com>
2025-04-15 16:28:20 -0500

    calculate_distance() created

--- code/main.py
+++ code/main.py
@@ -7,11 +7,13 @@ Author: Rahul Balasubramani(rahulb6) & Anushree Udhayakumar(au11)
 import random
 import matplotlib.pyplot as plt
 from geopy.distance import geodesic
+from geographiclib.geodesic import Geodesic
 
 # PERT function to generate sample
 def pert_sample(min_val, mode_val, max_val):
     """
     Generates a sample using PERT .
+    TODO: can you provide citations for the above formulas?
 
     :param min_val: float, worst-case value
     :param mode_val: float, most likely value
@@ -29,9 +31,23 @@ def pert_sample(min_val, mode_val, max_val):
     sample = random.betavariate(alpha, beta)
     return min_val + sample * (max_val - min_val)
 
+"""
 #Calculate distance between 2 points
 def calculate_distance(coord1, coord2):
     return geodesic(coord1, coord2).km
+"""
+def calculate_distance(lat1, lon1, lat2, lon2) -> float:
+    """
+    This function takes inputs of coordinates(lat-long) and computes the great-circle distance between.
+    :param lat1: Race location A latitude
+    :param lon1: Race location A longitude
+    :param lat2: Race location B latitude
+    :param lon2: Race location B longitude
+    :return: floating value that denotes the distance between the two points in meters.
+    TODO: make sure the return value makes sense in meters
+    """
+    distance_meters = Geodesic.WGS84.Inverse(lat1, lon1, lat2, lon2)['s12']
+    return distance_meters
 
 
 if __name__ == "__main__":
@@ -40,4 +56,6 @@ if __name__ == "__main__":
     for i in range(10):
         sample = pert_sample(min_val, mode_val, max_val)
         print(f"Sample {i+1}: {sample:.4f}")
-print("DONEEEEEEE")
+    print("DONEEEEEEE")
+    dist = calculate_distance(50.4372,5.9714, 47.58222, 19.25111)
+    print(dist)
